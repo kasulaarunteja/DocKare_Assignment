48fd76e756f1956c2aba5eff75690eac34b51f70
kasulaarunteja <kasulaarunteja@gmail.com>
2023-01-07 21:51:23 +0530

    form rou

--- src/Routes/FormRoues.jsx
+++ src/Routes/FormRoues.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Centeredtabs from '../Components/Centeredtabs';
 import Formheader from '../Components/Formheader';
+import QuestionForm from '../Components/Question_form';
 
 const FormRoues = () => {
     return (
         <>
          <Formheader/>
-         <Centeredtabs/>   
+         <Centeredtabs/>
+         <QuestionForm/> 
         </>
     );
 }
