941f1f5b093ef3476c605601a8d6a745944a282a
kasulaarunteja <kasulaarunteja@gmail.com>
2023-01-07 14:13:31 +0530

    app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './Components/Navbar';
+import Template from './Components/Template';
 
 function App() {
   return (
     <div className="">
       <Navbar/>
       <h2>hello world </h2>
+      <Template/>
     </div>
   );
 }
