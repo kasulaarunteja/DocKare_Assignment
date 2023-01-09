976e60b065bdbbc6258cac5b583a5e1273d023a7
kasulaarunteja <kasulaarunteja@gmail.com>
2023-01-09 13:20:59 +0530

    home

--- src/Components/Homepage.jsx
+++ src/Components/Homepage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import "./Homepage.css";
 import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 import { IconButton } from '@mui/material';
@@ -6,9 +6,32 @@ import StorageIcon from '@mui/icons-material/Storage';
 import FolderOpenIcon from '@mui/icons-material/FolderOpen';
 import SortByAlphaIcon from '@mui/icons-material/SortByAlpha';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import axios from 'axios';
+import { useNavigate } from 'react-router';
 
 
 const Homepage = () => {
+     
+      const [files, setFiles] = useState([]);
+      
+      async function filenames(){
+         let request = await axios.get("http://localhost:8080/get_all_filename")
+         let filenames = request.data;
+         setFiles(filenames)
+      }
+
+      filenames();
+
+
+      const navigate = useNavigate();
+       
+      function navigate_to(docname){
+         let fname = docname.split('.');
+         navigate("/form/" +fname[0]);
+      }
+      
+      
+     
     return (
         <div className='main_body'>
             <div className='mainbody_top'>
@@ -31,17 +54,27 @@ const Homepage = () => {
             </div>
             </div>
             <div className='mainbody_doc'>
-                 <div className='doc_card'>
-                    <img src="/images/docment.png" className='doc_image'/> 
-                    <div className='doc_card_content'>
-                        <div className='doc_content'>
-                            <div className='content_left'>
-                            <StorageIcon />
-                            </div>
-                             <MoreVertIcon/>
-                        </div>
-                    </div>
-                 </div>
+
+
+               {files.map((ele) => (
+                   <div className='doc_card'  
+                     onClick={() => {
+                        navigate_to(ele)
+                     }}
+                   >
+                   <img src="/images/docment.png" className='doc_image'/> 
+                   <div className='doc_card_content'>
+                     <h5 style={{overFlow:'ellipsis'}}>{ele ? ele : "Untitled Doc"}</h5>
+                       <div className='doc_content' style={{fontsize:'12px', color:'grey'}}>
+                           <div className='content_left'>
+                           <StorageIcon style={{color:'white',fontsize:'12px', backgrondColor:'#6E2594',padding:'3px',
+                        marginRight:'3px'}} />
+                           </div>
+                            <MoreVertIcon style={{fontSize:'16px',color:'grey'}}/>
+                       </div>
+                   </div>
+                </div>
+               ))}
             </div>
         </div>
     );
